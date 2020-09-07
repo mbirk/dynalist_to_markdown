0968d4e7e1d0067b2f94a0d2a65789560ebad4de
Michael Birk <michael@baotris.com>
2020-09-06 17:24:45 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,84 @@
+# Dynalist to Markdown
+
+This is a script that exports Dynalist to Markdown.  While it does generate
+standard Markdown, it was designed to export from Dynalist to Obsidian, and so
+it supports some Obsidian-specific features, such as internal links.
+
+## Installation
+
+The script requires Python 3, as well as a few python modules, such as
+`requests` and `pyyaml`.  I recommend creating a Python `venv` and installing
+modules in this environment.  This details of how to do this depend on your
+platform.  For example:
+
+```
+python3 -mvenv .venv
+. .venv/bin/activate
+pip install requests pyyaml
+```
+
+## Configuration
+
+Navigate to the [Dynalist developer page](https://dynalist.io/developer) and
+Generate a secret token.  Copy this secret token to your clipboard.
+
+Copy the `dynalist_to_markdown.yaml.example` file to
+`dynalist_to_markdown.yaml`, and open `dynalist_to_markdown.yaml` with a text
+editor.  Replace the `<YOUR API KEY HERE>` text with the secret token you just
+copied.
+
+Additionally, you can define the configuration on a per-page basis.  Since
+Dynalist is an outliner, each document is just a big nested list.  While we
+could just convert this into a big nested Markdown list, that's probably not
+what you want.
+
+[Note: A major weakness of the current implementation is that it only
+generates headings and list items -- never paragraphs.  Hopefully a future
+version will provide this functionality.]
+
+Edit the `pages` section of the `dynalist_to_markdown.yaml` config file,
+using the following configuration settings.
+
+| Key                       | Description                                    |
+| ------------------------- | ---------------------------------------------- |
+| `name`                    | The name of the page to which these configuration settings apply.  This can be a pattern, using a `*` to match any sequence of characters (or `?` to match a single character). |
+| `heading_depth`           | The number of levels of headings to generate. |
+| `ignore`                  | Ignore this page -- don't generate Markdown for it. |
+| `include_notes`           | Whether to include the "notes" section of a Dynalist item. |
+| `obsidian_internal_links` | Whether to convert internal links to Obsidian-style notation (e.g. `[[Link]]`) |
+
+## Usage
+
+Once you've created the configuration file, you just simply run the script.  There are
+a few optional command-line settings, however:
+
+```
+$ python dynalist_to_markdown.py -h
+usage: dynalist_to_markdown.py [-h] [--config CONFIG] [--directory DIRECTORY]
+                               [--overwrite] [--cache]
+
+Export Dynalist to markdown
+
+optional arguments:
+  -h, --help             show this help message and exit
+  --config CONFIG        Path to config file (default:
+                         ./dynalist_to_markdown.yaml)
+  --directory DIRECTORY  Directory for output (default: dynalist)
+  --overwrite            Overwrite files in output directory
+  --cache                Enable requests cache (useful for development)
+```
+
+By default, the script will create a folder called `dynalist` with all of the
+generated Markdown files.  To change this folder name, use the `--directory`
+option.
+
+If you plan to run the script multiple times (e.g. tweaking the configuration
+each time, or modifying the Python script), you may want to enable request
+caching.  This enables you to run the script subsequent times without hitting
+the Dynalist API each time.  To do this, install the `requests_cache` module
+and use the `--cache option`:
+
+```
+pip install requests_cache
+python dynalist_to_markdown.py --cache
+```
---
+++ dynalist_to_markdown.py
@@ -0,0 +1,218 @@
+import dataclasses, fnmatch, os.path, pathlib, re, requests, yaml
+
+class DynalistToMarkdown:
+    api_base = 'https://dynalist.io/api/v1/'
+
+    def __init__(self, config,
+        directory    = 'dynalist',
+        overwrite    = False,
+        http_session = requests
+    ):
+        self.directory    = directory
+        self.overwrite    = overwrite
+        self.http_session = http_session
+
+        config = yaml.safe_load(config)
+        self.api_key      = config['api_key']
+        self.page_configs = [
+            self.PageConfig(**page)
+            for page in config['pages']
+        ]
+
+    def export(self):
+        if not self.overwrite and os.path.exists(self.directory):
+            raise Exception('Directory exists: ' + self.directory)
+
+        self.fetch_files()
+        self.process_files()
+
+    def document_to_markdown(self, path, node_by_id, config, out):
+        for context in self.traverse_nodes(node_by_id, 'root'):
+            node    = context[-1]
+            content = node['content']
+
+            if config.obsidian_internal_links:
+                content = self.replace_markdown_links(content)
+
+            if len(context) <= config.heading_depth:
+                if len(context) > 1:
+                    print(file=out)
+ 
+                depth   = len(context)
+                heading = '#' * depth
+                print(f'{heading} {content}', file=out)
+ 
+            else:
+                if config.include_notes:
+                    note = node.get('note', '')
+                    if note != '':
+                        content += f' ({note})'
+
+                indent = len(context) - config.heading_depth - 1
+                indent = '\t' * indent
+                print(f'{indent}- {content}', file=out)
+
+    def fetch_files(self):
+        response = self.request('file/list')
+        files    = response['files']
+
+        self.file_by_id   = self.index_nodes(files)
+        self.root_file_id = response['root_file_id']
+
+    def get_document(self, document_id):
+        return self.request('doc/read', file_id=document_id)
+
+    def page_config(self, page):
+        config = None
+        for page_config in self.page_configs:
+            if page_config.match(page):
+                if config:
+                    config = config.merge(page_config)
+                else:
+                    config = page_config
+        return config
+
+    def process_files(self):
+        for context in self.traverse_nodes(self.file_by_id, self.root_file_id):
+            path = '/'.join(c['title'] for c in context[1:])
+            node = context[-1]
+            type = node['type']
+
+            config = self.page_config(path)
+            if type == 'document' and not config.ignore:
+                self.process_document(path, node, config)
+
+    def process_document(self, path, node, config):
+        document_id = node['id']
+        document    = self.get_document(document_id)
+        nodes       = document['nodes']
+        node_by_id  = self.index_nodes(nodes)
+
+        print(f'{self.directory}/{path}')
+        dir = os.path.dirname(path)
+        pathlib.Path(self.directory, dir).mkdir(parents=True, exist_ok=True)
+        with open(f'{self.directory}/{path}.md', 'w') as out:
+            self.document_to_markdown(path, node_by_id, config, out)
+
+    def request(self, api_path, **parameters):
+        url = self.api_base + api_path
+
+        response = self.http_session.post(url, json={'token': self.api_key, **parameters})
+        if response.status_code != 200:
+            raise Exception(f'Dynalist API error: {response.status_code}')
+
+        result = response.json()
+        if result.get('_code') == 'InvalidToken':
+            raise Exception('Invalid API key: see https://dynalist.io/developer')
+        return result
+
+    def index_nodes(self, nodes):
+        return {
+            node['id']: node
+            for node in nodes
+        }
+
+    def traverse_nodes(self, node_by_id, root_id):
+        def traverse(context, id):
+            node    = node_by_id[id]
+            context = context + [node]
+            yield context
+
+            children = node.get('children')
+            if children:
+                for child_id in children:
+                    yield from traverse(context, child_id)
+
+        return traverse([], root_id)
+
+    markdown_link_re = re.compile(r'\[([^\[]+)\]\(([^\)]+)\)')
+    def replace_markdown_links(self, s):
+        return re.sub(self.markdown_link_re, self.replace_obsidian_internal_links, s)
+
+    # TODO: match fragment part and link to heading?
+    dynalist_document_link_re = re.compile(r'^https://dynalist.io/d/([^#]+)')
+    def replace_obsidian_internal_links(self, group):
+        url       = group[2]
+        url_group = self.dynalist_document_link_re.match(url)
+        if url_group:
+            document_id = url_group[1]
+            document    = self.file_by_id.get(document_id)
+            if document:
+                text  = group[1]
+                title = document['title']
+                if text == title:
+                    return f'[[{title}]]'
+                else:
+                    return f'[[{title}|{text}]]'
+        return group[0]
+
+    @dataclasses.dataclass
+    class PageConfig:
+        name                    : str
+        heading_depth           : int  = None
+        ignore                  : bool = None
+        include_notes           : bool = None
+        obsidian_internal_links : bool = None
+
+        def match(self, page):
+            return fnmatch.fnmatch(page, self.name)
+
+        def merge(self, page_config):
+            changes = {
+                key: value
+                for key, value in dataclasses.asdict(page_config).items()
+                if value is not None
+            }
+            return dataclasses.replace(self, **changes)
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description     = 'Export Dynalist to markdown',
+        formatter_class = lambda prog: argparse.HelpFormatter(prog, max_help_position=40, width=79),
+    )
+
+    parser.add_argument(
+        '--config',
+        help     = 'Path to config file (default: ./dynalist_to_markdown.yaml)',
+        default  = 'dynalist_to_markdown.yaml',
+    )
+
+    parser.add_argument(
+        '--directory',
+        help     = 'Directory for output (default: dynalist)',
+        default  = 'dynalist',
+    )
+
+    parser.add_argument(
+        '--overwrite',
+        help     = 'Overwrite files in output directory',
+        action   = 'store_true',
+    )
+
+    parser.add_argument(
+        '--cache',
+        help     = 'Enable requests cache (useful for development)',
+        action   = 'store_true',
+    )
+
+    args = parser.parse_args()
+
+    if args.cache:
+        import requests_cache
+        http_session = requests_cache.CachedSession(
+            cache_name        = 'dynalist_exporter',
+            allowable_methods = ('GET', 'POST')
+        )
+    else:
+        http_session = requests
+
+    with open(args.config) as config:
+        exporter = DynalistToMarkdown(
+            config       = config,
+            directory    = args.directory,
+            overwrite    = args.overwrite,
+            http_session = http_session,
+        )
+        exporter.export()
